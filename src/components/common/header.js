3e7218586a25a7eda61eba819d0ad8aa4dbf3670
Mutti u rehman <mutti.rehman@adib.com>
2025-04-23 16:40:40 +0400

    previous list of user fetch requet setup

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,7 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
 const Header = () => { 
-    return <>
-        <h2 className="text-3xl font-bold underline">Testing</h2>
-    </>
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+                const get_all_users_url = `${BASE_URL}/users`;
+                const res = await fetch(get_all_users_url);
+                console.log(BASE_URL);
+                const data = await res.json();  // <== you need this
+                console.log(data);
+                if (data?.length)
+                    setUsers(data);
+            } catch (err) {
+                console.error('Error fetching data:', err);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <h2>List of Users</h2>
+            <ul>
+                {
+                    users.map((user, index) => (
+                        <li key={index}>
+                            name: {user?.name || ''}</li>
+                    ))
+                }
+            </ul>
+        </>
+    );
 }
 
 export default Header;

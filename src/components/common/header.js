29651dec0c48bb10c9f3e64bf336008c4412c40b
Mutti u rehman <mutti.rehman@adib.com>
2025-04-25 11:57:09 +0400

    added images

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import MENU from './menu.constant';
+import { Link } from 'react-router';
+import {
+    BeakerIcon,
+    UserIcon,
+    CodeBracket,
+    AcademicCapIcon
+} from '@heroicons/react/24/solid';
+
 
 const Header = () => { 
     const [users, setUsers] = useState([]);
+    const menuList = MENU; 
 
     useEffect(() => {
         async function fetchData() {
@@ -24,15 +34,29 @@ const Header = () => {
 
     return (
         <>
-            <h2>List of Users</h2>
-            <ul>
-                {
-                    users.map((user, index) => (
-                        <li key={index}>
-                            name: {user?.name || ''}</li>
-                    ))
-                }
-            </ul>
+            <header className='flex justify-between items-center'>
+                <div className=''>
+                    <p>My Profile</p>
+                </div>
+
+                <div className='flex gap-4'>
+                    {menuList.map((menu, index) => {
+                        const { name, icon, link, title } = menu;
+                        const iconAddress = '/assets/icons/';
+                        return (
+                            <div key={index} className='flex items-center gap-2'>
+                                <img
+                                    src='/public/assets/icons/setting.svg'
+                                    // src={`${iconAddress}${icon}`}
+                                    // alt={`${title} icon`}
+                                    // className="size-6 text-blue-500"
+                                />
+                                <Link to={link} className='text-sm'>{title}</Link>
+                            </div>
+                        );
+                    })}
+                </div>
+            </header>
         </>
     );
 }
---
+++ src/components/common/menu.constant.js
@@ -0,0 +1,61 @@
+
+const MENU = [
+    {
+        name: 'summary',
+        link: '/summary',
+        title: 'Summary',
+        icon: 'setting.svg',
+        description: 'Summary of your profile'
+    },
+    {
+        name: 'education',
+        link: '/education',
+        title: 'Education',
+        icon: 'academicCap.svg',
+        description: 'Education details'
+    },
+    {
+        name: 'experience',
+        link: '/experience',
+        title: 'Experience',
+        icon: 'briefcase.svg',
+        description: 'Experience details'
+    },
+    {
+        name: 'skills',
+        link: '/skills',
+        title: 'Skills',
+        icon: 'sparkles.svg',
+        description: 'Skills details'
+    },
+    {
+        name: 'projects',
+        link: '/projects',
+        title: 'Projects',
+        icon: 'folder.svg',
+        description: 'Projects details'
+    },
+    {
+        name: 'certificates',
+        link: '/certificates',
+        title: 'Certificates',
+        icon: 'trophyIcon.svg',
+        description: 'Certificates details'
+    },
+    {
+        name: 'languages',
+        link: '/languages',
+        title: 'Languages',
+        icon: 'languageIcon.svg',
+        description: 'Languages details'
+    },
+    {
+        name: 'hobbies',
+        link: '/hobbies',
+        title: 'beakerIcon',
+        icon: 'hobbyIcon.svg',
+        description: 'Hobbies details'
+    }
+];
+
+export default MENU;
